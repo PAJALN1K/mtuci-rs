07c31b5a40141759698b921b3befed907e2091eb
springguy <p4j4ln1k@gmail.com>
2023-07-15 22:55:08 +0300

    A fix

--- chapter_13_1_closures/src/main.rs
+++ chapter_13_1_closures/src/main.rs
@@ -178,28 +178,28 @@
 
 // Попытка использовать замыкание FnOnce с sort_by_key
 
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
 
-fn main() {
-    let mut list = [
-        Rectangle { width: 10, height: 1 },
-        Rectangle { width: 3, height: 5 },
-        Rectangle { width: 7, height: 12 },
-    ];
+// fn main() {
+//     let mut list = [
+//         Rectangle { width: 10, height: 1 },
+//         Rectangle { width: 3, height: 5 },
+//         Rectangle { width: 7, height: 12 },
+//     ];
 
-    let mut sort_operations = vec![];
-    let value = String::from("by key called");
+//     let mut sort_operations = vec![];
+//     let value = String::from("by key called");
 
-    list.sort_by_key(|r| {
-        sort_operations.push(value);
-        r.width
-    });
-    println!("{:#?}", list);
-}
+//     list.sort_by_key(|r| {
+//         sort_operations.push(value);
+//         r.width
+//     });
+//     println!("{:#?}", list);
+// }
 
 
 // Пример 9
